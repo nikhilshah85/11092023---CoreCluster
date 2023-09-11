0ac2da01846eb6b696443ddbbfb6b0194b056902
Nikhil <nikhilshah.net@hotmail.com>
2023-09-11 10:51:04 +0530

    Day 1 - collections

---
+++ Day 1/collectionsDemo/collectionsDemo/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collectionsDemo
+{
+    internal class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public string EmpDesignation { get; set; }
+        public double EmpSalary { get; set; }
+        public bool EmpIsPermenant { get; set; }
+
+    }
+}
---
+++ Day 1/collectionsDemo/collectionsDemo/Program.cs
@@ -0,0 +1,126 @@
+﻿
+
+using collectionsDemo;
+using System.Collections;
+
+#region Array Demo
+
+
+////string technology = new string(); //single variable, cannot store multiple values
+//string[] technologies = new string[5];
+//technologies[0] = ".Net";
+//technologies[1] = "Angular";
+//technologies[2] = "Azure";
+//technologies[3] = "React";
+//technologies[4] = "SQL Server";
+
+////foreach (var item in technologies)
+////{
+////    Console.WriteLine(item);
+////}
+
+
+//int[] favNumers = new int[10];
+
+//int evenNumbers = 0;
+//int oddNumber = 0;
+//int negativeNumbers = 0;
+//int positiveNumbers = 0;
+//int total = 0;
+
+//foreach(int i in favNumers)
+//{
+//    Console.WriteLine("Enter A Number");
+//    favNumers[i] = Convert.ToInt32(Console.ReadLine());
+
+//    if (favNumers[i] > 0)
+//    {
+//        positiveNumbers = positiveNumbers + 1;
+//        if (favNumers[i] % 2 == 0)
+//        {
+//            evenNumbers = evenNumbers + 1;
+
+//        }
+//        else
+//        {
+//            oddNumber = oddNumber + 1;
+//        }
+//    }
+//    else
+//    {
+//        negativeNumbers = negativeNumbers + 1;   
+//    }
+
+//    total = total + favNumers[i];
+//}
+
+//Console.WriteLine("Total Numbers " + favNumers.Length);
+//Console.WriteLine("Sum of Numbers " + total);
+//Console.WriteLine("Even Numbers " + evenNumbers);
+
+//Console.WriteLine("Odd Numbers " + oddNumber);
+
+//Console.WriteLine("Positive Numbers " + positiveNumbers);
+//Console.WriteLine("Negative Numbers " + negativeNumbers);
+
+
+
+
+
+
+
+#endregion
+
+#region ArrayList
+
+
+
+
+//ArrayList favNumbers = new ArrayList();
+
+//favNumbers.Add(20);
+//favNumbers.Add(20.90);
+//favNumbers.Add("Ten");
+//favNumbers.Add("Twenty");
+//favNumbers.Add(true);
+//favNumbers.Add(false);
+
+//favNumbers.Add(new Employee() { EmpNo = 101, EmpName = "Nikhil", EmpDesignation = "Sales", EmpIsPermenant = true, EmpSalary = 5000 });
+
+//foreach (var item in favNumbers)
+//{
+//    Console.WriteLine(item);
+
+//	if (item.GetType() == typeof(Employee))
+//	{
+//		Employee e =(Employee) item;
+//		Console.WriteLine(e.EmpNo);
+//        Console.WriteLine(e.EmpName);
+//        Console.WriteLine(e.EmpDesignation);
+//        Console.WriteLine(e.EmpSalary);
+//        Console.WriteLine(e.EmpIsPermenant);
+
+//    }
+//}
+
+#endregion
+
+#region List
+
+//List<string> list = new List<string>();
+//list.Add(".Net");
+//list.Add("Java");
+//list.Add("Angular");
+//list.Add("React");
+//list.Add("Azure");
+//list.Add("AWS");
+
+#endregion
+
+
+
+
+
+
+
+
