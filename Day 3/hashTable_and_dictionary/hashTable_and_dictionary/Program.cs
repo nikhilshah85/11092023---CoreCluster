8947450c4bea1c7d80df4515444240035524dcb2
Nikhil <nikhilshah.net@hotmail.com>
2023-09-13 10:56:40 +0530

    Day 3

---
+++ Day 3/hashTable_and_dictionary/hashTable_and_dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region Hashtable
+//Hashtable myList = new Hashtable();
+
+//myList.Add(1, "Nikhil");
+//myList.Add(2, "Kriti");
+//myList.Add(3, "Sumit");
+//myList.Add(4, "Mohan");
+//myList.Add(5, "Ishan");
+//myList.Add("Six", 40.90);
+
+//foreach (var item in myList.Values)
+//{
+//    Console.WriteLine(item);
+//}
+#endregion
+
+#region Dictionary
+
+//Dictionary<int, string> mylist = new Dictionary<int, string>();
+//mylist.Add(1, "Nikhil");
+
+
+//foreach (var item in mylist)
+//{
+//    Console.WriteLine(item.Value);
+//}
+
+#endregion
